2d81b1082c8bc38fe8f91bb5bee93cf6ecf1c225
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2022-02-02 19:31:56 +0530

    build script change, trying without terser

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     input: 'src/index.js',
     output: {
         file: 'dist/wordle.js',
-        format: 'es',
-        plugins: [terser()]
+        format: 'es'
+        // plugins: [terser()]
     }
 };
