589f44d9517afbe00d718a1af742c7365c2bb891
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2022-02-02 19:53:31 +0530

    shebang banner for npx execution

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ export default {
     input: 'src/index.js',
     output: {
         file: 'dist/wordle.js',
+        banner: '#!/usr/bin/env node',
         format: 'es',
         plugins: [terser()]
     }
